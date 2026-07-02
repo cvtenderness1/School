4463478907e2bdfe41114ac66d19cedec08d1392
cvtenderness1 <3247717916@qq.com>
2026-07-02 12:43:13 +0800

    用户小程序端

--- 骑手端/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
-export default defineConfig({
-  build: {
-    sourcemap: process.env.NODE_ENV === 'development',
-  },
-  plugins: [uni()],
-})
